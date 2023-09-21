2c4ddf0e7a3a4ae340eef3ee1b583a43bd1f6214
WPTENG396\José Neto <jose.neto@alticeusa.com>
2023-09-21 13:11:49 -0300

    Implementação do JDK 11

--- src/main/java/com/azure/model/AzureModel.java
+++ src/main/java/com/azure/model/AzureModel.java
@@ -1,12 +1,44 @@
 package com.azure.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class AzureModel {
 	private String nome;
 	private String sobreNome;
 	private String cidadeNascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+
+	public String getCidadeNascimento() {
+		return cidadeNascimento;
+	}
+
+	public void setCidadeNascimento(String cidadeNascimento) {
+		this.cidadeNascimento = cidadeNascimento;
+	}
+
+	public AzureModel(String nome, String sobreNome, String cidadeNascimento) {
+		super();
+		this.nome = nome;
+		this.sobreNome = sobreNome;
+		this.cidadeNascimento = cidadeNascimento;
+	}
+
+	@Override
+	public String toString() {
+		return "AzureModel [nome=" + nome + ", sobreNome=" + sobreNome + ", cidadeNascimento=" + cidadeNascimento + "]";
+	}
+
 }
